231d6d81ed31eb30bffd38696d646ad719713950
Miłosz Kramek <milosz.kramek@gmail.com>
2023-07-27 20:59:59 +0200

    Dodana tabela do kursu 01

--- course-01/app.js
+++ course-01/app.js
@@ -1,6 +1,7 @@
 // 1. Import modułu odpowiedzialnego za obsługę plików
 const fs = require("node:fs");
 const promiseFs = require("node:fs").promises;
+const path = require("node:path");
 const { print } = require("./externalModule");
 
 // 2. Wykorzystanie wbudowanej funkcji do odczytu plików
@@ -17,18 +18,21 @@ fs.readdir(__dirname, (error, files) => {
 
 // Wariant 2: sync
 try {
-    const list = fs.readdirSync(__dirname);
-    print("(sync) Lista plików: ", list);
+  const list = fs.readdirSync(__dirname);
+  print("(sync) Lista plików: ", list);
 } catch (error) {
-    print("Wystąpił błąd: ", error);
+  print("Wystąpił błąd: ", error);
 }
 
 // Wariant 3: Async + Promise
-promiseFs.readdir(__dirname).then((list) => {
+promiseFs
+  .readdir(__dirname)
+  .then((list) => {
     print("(async + promise) Lista plików: ", list);
-}).catch((error) => {
+  })
+  .catch((error) => {
     print("Wystąpił błąd: ", error);
-})
+  });
 
 // Zmienna globalna - dostępna na obszarze CAŁEJ aplikacji
 global.sampleNumber = 10;
@@ -53,3 +57,19 @@ process.nextTick(() => {
   print("nextTick uruchomiony");
 });
 
+promiseFs
+  .readdir(__dirname)
+  .then((files) => {
+    return Promise.all(
+      files.map(async (filename) => {
+        const stats = await promiseFs.stat(path.join(__dirname, filename));
+        console.log(stats);
+        return {
+          "Nazwa pliku": filename,
+          "Rozmiar pliku": stats.size,
+          "Data modyfikacji": stats.mtime,
+        };
+      })
+    );
+  })
+  .then((result) => console.table(result));
